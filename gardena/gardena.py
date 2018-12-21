4f453c07506a9db9c1abb024eb257ac27644454f
Andreas Hartl <home-assistant@nd-com.net>
2018-12-21 14:16:24 +0100

    Retrieving locations

--- gardena/gardena.py
+++ gardena/gardena.py
@@ -1,9 +1,7 @@
-class NotLoggedInException(Exception):
-    """Not logged to Gardena Web Services Exception."""
+import logging
+import requests
 
-    def __init__(self):
-        super(NotLoggedInException, self).__init__()
-
+_LOGGER = logging.getLogger(__name__)
 
 def defaults_namedtuple(name, fields):
     from collections import namedtuple
@@ -13,41 +11,63 @@ def defaults_namedtuple(name, fields):
 
 Session = defaults_namedtuple("Session", "token user_id refresh_token")
 
+class Location:
+    
+    def __init__(self, id, name=None, devices=None, **kw):
+        self.id = id
+        self.name = name
+        self.devices = devices or []
+    
+    def __repr__(self):
+        return "Location({})".format(", ".join(repr(i) for i in (self.id, self.name, self.devices)))
+
 class Hub:
     BASE_URL = "https://smart.gardena.com/sg-1"
 
     session = Session()
+    requestSession = None
     
     def __init__(self, username, password):
         self.username = username
         self.password = password
-
-    def execute(self, path, data, useSession=True):
-        import requests
-        import json
+        self.requestSession = requests.Session()
+    
+    def execute(self, path, params=None, data=None, method=None):
         headers = {
-            "Content-Type": "application/json",
             "Accept": "application/json",
         }
-        if useSession:
-            if not self.session.token:
-                raise NotLoggedInException()
-            headers["X-Session"] = self.session.token
-        return requests.post(
+        
+        if not method:
+            method = "get" if data is None else "post"
+        response = self.requestSession.request(
+            method,
             url=self.BASE_URL + path,
+            params=params,
             headers=headers,
-            data=json.dumps(data) if data is not None else None
+            json=data,
+            allow_redirects=True,
         )
+        print(response.content)
+        return response
     
     def login(self):
+        _LOGGER.info("Logging in with user name %s", self.username)
+        self.requestSession = requests.Session()
         data = {
             "sessions": {
                 "email": self.username,
                 "password": self.password,
             }
         }
-        response = self.execute("/sessions", data, useSession=False)
+        response = self.execute("/sessions", data=data)
         json = response.json()
         self.session = Session(**json.get("sessions", {}))
-        if not self.session.token:
-            raise NotLoggedInException()
+        _LOGGER.info("Got a session %s", self.session)
+        self.requestSession.headers["X-Session"] = self.session.token
+    
+    def retrieve_locations(self):
+        _LOGGER.info("Retrieving locations for user id %s", self.session.user_id)
+        response = self.execute("/locations", {"user_id": self.session.user_id})
+        data = response.json()
+        _LOGGER.info("Got locations %s", data)
+        return [Location(**location) for location in data.get("locations", [])]
